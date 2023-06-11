34a30ba9ab274e6a7f09a4b31b94deaf6bc67a91
ForestDr <nikita2001395@gmail.com>
2023-06-11 23:10:49 +0300

    add default home page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,17 @@ import { Layout } from "components/Layout";
 const RegisterPage = lazy(() => import("pages/Register/Register"));
 const LogInPage = lazy(() => import("pages/LogIn/LogIn"));
 
+const HomePage = () => (
+  <div>
+    Home Page <Link to="/register">register</Link>
+  </div>
+);
+
 export const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Link to="/register">register</Link>} />
+        <Route index element={<HomePage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LogInPage />} />
       </Route>
