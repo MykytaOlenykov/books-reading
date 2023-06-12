2a319c9e5ebef27e2fb9999bfe85f193ea359790
ForestDr <nikita2001395@gmail.com>
2023-06-12 11:12:37 +0300

    remove lazy components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "components/Layout";
+import Home from "pages/Home";
+import Register from "pages/Register/Register";
+import LogIn from "pages/LogIn/LogIn";
 
-const HomePage = React.lazy(() => import("pages/Home"));
+// const HomePage = React.lazy(() => import("pages/Home"));
 
-const RegisterPage = React.lazy(() => import("pages/Register/Register"));
+// const RegisterPage = React.lazy(() => import("pages/Register/Register"));
 
-const LogInPage = React.lazy(() => import("pages/LogIn/LogIn"));
+// const LogInPage = React.lazy(() => import("pages/LogIn/LogIn"));
 
 export const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LogInPage />} />
+        <Route index element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<LogIn />} />
       </Route>
     </Routes>
   );
