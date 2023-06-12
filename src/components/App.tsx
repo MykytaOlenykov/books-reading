f940186965d303c9756c7c53ada8a7cb5ee9e0cd
ForestDr <nikita2001395@gmail.com>
2023-06-12 10:35:43 +0300

    test commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,9 @@ import { Layout } from "components/Layout";
 // const RegisterPage = lazy(() => import("pages/Register/Register"));
 // const LogInPage = lazy(() => import("pages/LogIn/LogIn"));
 
-const Header = () => (
+const HomePage = () => (
   <div>
+    <div>Home page</div>
     <header>
       <Link to="/">Home</Link>
       <Link to="/register">register</Link>
@@ -18,28 +19,14 @@ const Header = () => (
   </div>
 );
 
-const HomePage = () => <div>Home page</div>;
-
 const RegisterPage = () => <div>Register page</div>;
 
 const LogInPage = () => <div>LogIn page</div>;
 
-// export const App: React.FC = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<HomePage />} />
-//         <Route path="register" element={<RegisterPage />} />
-//         <Route path="login" element={<LogInPage />} />
-//       </Route>
-//     </Routes>
-//   );
-// };
-
 export const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Header />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LogInPage />} />
