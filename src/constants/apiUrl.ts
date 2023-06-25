28eec62226af5c5beef82087a568468d2fc54aec
ForestDr <nikita2001395@gmail.com>
2023-06-25 13:03:00 +0300

    fix

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,5 +1,6 @@
 // const GOIT_URL = "https://bookread-backend.goit.global";
 const LOCAL_URL = "http://localhost:8000";
-// const MY_BACK = "https://books-reading-backend-ql7x.onrender.com";
+const MY_BACK = "https://books-reading-backend-ql7x.onrender.com";
 
-export const API_URL = LOCAL_URL;
+export const API_URL =
+  process.env.NODE_ENV === "development" ? LOCAL_URL : MY_BACK;
